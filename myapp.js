ed8acd88553ea27762b081103363160655816282
Naresh <72507640+White-Town@users.noreply.github.com>
2025-06-13 18:32:47 +0545

    Update myapp.js
    update myapp v2

--- myapp.js
+++ myapp.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Node.js App! i am naresh ');
+  res.send('Hello World from Node.js App! i am naresh. jenkins  ');
 });
 
 app.listen(port, () => {
